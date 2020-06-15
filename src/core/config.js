339602def133f5a3b78ef897c94503b66425901e
SuperAdam47 <hermelindo.lopez0714@gmail.com>
2020-06-16 02:53:17 +1000

    firebase config protect
    firebase config

--- src/core/config.js
+++ src/core/config.js
@@ -1,8 +1,8 @@
 // Replace with your own firebase config!
 export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBKY8N7jLl98R7RC8v4pdl1F0tDm48nAdY",
-  databaseURL: "https://react-native-market-2861b.firebaseio.com",
-  projectId: "videochat-a6273",
-  storageBucket: "videochat-a6273.appspot.com",
-  appId: "1:448729484846:android:ebc029d8604b4e04ae59bf",
+  apiKey: "YOUR API LEY",
+  databaseURL: "YOUR DB URL",
+  projectId: "APP ID",
+  storageBucket: "STORAGE BUCKET",
+  appId: "APP ID",
 };
